aefbe6b874bdfeca270d004d8499dd2837de4261
lsfhv <Lsfhv@protonmail.com>
2022-11-26 00:48:40 +0000

    cross product of lists

--- main.scala
+++ main.scala
@@ -5,8 +5,8 @@ case object Q2 extends State
 case object Q3 extends State
 case object Q4 extends State
 
-val states: List[State] = Q0::Q1::Q2::Nil
-val finalStates: List[State] = Q1::Nil
+val states: List[State] = Q0::Q1::Q2::Q3::Q4::Nil
+val finalStates: List[State] = Q4::Nil
 val alphabet: List[Char] = '1'::'0'::Nil
 
 case class DFA(states: List[State],
@@ -27,16 +27,30 @@ case class DFA(states: List[State],
 // example dfa, string starting with 1
 
 val transition : (State, Char) => State = {
-    case (Q0, '1') => Q1
-    case (Q0, '0') => Q2
-    case (Q2, _) => Q2
-    case (Q1, _) => Q1
+    case (Q0, '1') => Q2
+    case (Q0, '0') => Q1
+    case (Q1, '0') => Q4
+    case (Q1, '1') => Q2
+    case (Q2, '0') => Q3
+    case (Q2, '1') => Q2
+    case (Q3, '0') => Q4
+    case (Q3, '1') => Q1
+    case (Q4, _) => Q4
 }
 
 val dfa = new DFA(states, Q0, transition, finalStates)
 
 // minimize the DFA
 
-// calcualte the transiton monoid
+implicit class Crossable[X](xs: List[X]) {
+    def cross[Y](ys: List[Y]) = 
+        (for (i <- 0 until xs.size) yield 
+            for (s <- (for (j <- i+1 until ys.size) yield ys(j)).toList) yield (xs(i), s)).toList
+}
+
+// initaliit ymark the states
+
+(states cross states).filter()
+
 
 // check if its aperiodic
